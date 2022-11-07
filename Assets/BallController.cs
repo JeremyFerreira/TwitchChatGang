7a4353cc368fe08be357975ef8a6cc511b55cb61
JeremyFerreira <91945288+JeremyFerreira@users.noreply.github.com>
2022-11-07 21:23:49 +0100

    reset ballPos

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -8,6 +8,8 @@ public class BallController : MonoBehaviour
     [SerializeField] float startSpeed;
     [SerializeField] float currentSpeed;
     [SerializeField] float speedMultiplier;
+    [SerializeField] float boundaryRightX;
+    [SerializeField] float boundaryLeftX;
     public void AddBallSpeed()
     {
         currentSpeed = currentSpeed*speedMultiplier;
@@ -18,28 +20,41 @@ public class BallController : MonoBehaviour
     {
         ballRB = GetComponent<Rigidbody2D>();
 
-        //gets a random angle of the ball to start
-        float angle = Random.Range(1, 101);
-        float dir = Random.Range(-1, 1);
-        if(dir == 0)
-        {
-            dir = 1;
-        }
-
-        Vector2 randomDirection = new Vector2(dir,angle/100);
-        randomDirection.Normalize();
-        Debug.Log(randomDirection);
-
-        
-        
-        ballRB.velocity = randomDirection * startSpeed;
-
-        currentSpeed = startSpeed;
+        LaunchBall();
     }
 
     // Update is called once per frame
     void Update()
     {
         ballRB.velocity = ballRB.velocity.normalized * currentSpeed;
+
+        if(transform.position.x > boundaryRightX)
+        {
+            transform.position = Vector3.zero;
+            LaunchBall();
+        }
+        if (transform.position.x < boundaryLeftX)
+        {
+            transform.position = Vector3.zero;
+            LaunchBall();
+        }
+
+    }
+    public void LaunchBall()
+    {
+        //gets a random angle of the ball to start
+        float angle = Random.Range(1, 101);
+        float dir = Random.Range(-1, 1);
+        if (dir == 0)
+        {
+            dir = 1;
+        }
+
+        Vector2 randomDirection = new Vector2(dir, angle / 100);
+        randomDirection.Normalize();
+
+        ballRB.velocity = randomDirection * startSpeed;
+
+        currentSpeed = startSpeed;
     }
 }
