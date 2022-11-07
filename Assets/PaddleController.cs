c1e4e92b7c3e09fc1d412bff7e1c73e816fc56ee
JeremyFerreira <91945288+JeremyFerreira@users.noreply.github.com>
2022-11-07 18:47:22 +0100

    ohh tu comprend pas pull cque ca veut dire ???

---
+++ Assets/PaddleController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    [SerializeField] Transform[] PositionsY;
+    [SerializeField] Transform paddleLeft;
+    [SerializeField] Transform paddleRight;
+    [Range(0, 7)]
+    [SerializeField] int positionPaddleLeft;
+    [Range(0, 7)]
+    [SerializeField] int positionPaddleRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangePosPaddleLeft(positionPaddleLeft);
+        ChangePosPaddleRight(positionPaddleRight);
+    }
+    public void ChangePosPaddleLeft(int pos)
+    {
+        paddleLeft.position = new Vector2(paddleLeft.position.x, PositionsY[pos].position.y);
+    }
+    public void ChangePosPaddleRight(int pos)
+    {
+        paddleRight.position = new Vector2(paddleRight.position.x,PositionsY[pos].position.y);
+    }
+}
