e63904b22303089b0755388f7194bf7b8a2bf7a2
JeremyFerreira <91945288+JeremyFerreira@users.noreply.github.com>
2022-11-08 19:28:51 +0100

    paddle

--- Assets/Script/PaddleController.cs
+++ Assets/Script/PaddleController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class PaddleController : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class PaddleController : MonoBehaviour
     [SerializeField] int positionPaddleLeft;
     [Range(0, 7)]
     [SerializeField] int positionPaddleRight;
+    [SerializeField] Vector2 posToGoRight;
+    [SerializeField] Vector2 posToGoLeft;
+    public float timeSinceLastMove;
+    [SerializeField] float speed;
 
     private void Awake()
     {
@@ -20,23 +25,34 @@ public class PaddleController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeSinceLastMove = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //ChangePosPaddleLeft(positionPaddleLeft);
-        //ChangePosPaddleRight(positionPaddleRight);
+        if (timeSinceLastMove < 1)
+        {
+            Debug.Log("yo");
+            paddleLeft.transform.position = new Vector2(paddleLeft.transform.position.x, Vector2.Lerp(transform.position, posToGoLeft, timeSinceLastMove).y);
+            paddleRight.transform.position = new Vector2(paddleRight.transform.position.x, Vector2.Lerp(transform.position, posToGoRight, timeSinceLastMove).y);
+            timeSinceLastMove += Time.deltaTime * speed;
+        }
+        else
+        {
+            paddleLeft.transform.position = new Vector2(paddleLeft.position.x, posToGoLeft.y);
+            paddleRight.transform.position = new Vector2(paddleRight.position.x, posToGoRight.y);
+        }
     }
-    
     public void ChangePosPaddleLeft(int pos)
     {
-        Debug.Log("lel");
-        paddleLeft.position = new Vector2(paddleLeft.position.x, PositionsY[pos].position.y);
+        posToGoLeft = new Vector2(paddleLeft.position.x, PositionsY[pos].position.y);
+        timeSinceLastMove = 0;
     }
     public void ChangePosPaddleRight(int pos)
     {
-        paddleRight.position = new Vector2(paddleRight.position.x,PositionsY[pos].position.y);
+        posToGoRight = new Vector2(paddleRight.position.x, PositionsY[pos].position.y);
+        timeSinceLastMove = 0;
     }
 }
+
